f1e5defcbedb2b678204c89b684233c23c014a29
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-03-20 18:43:58 +0530

    Add files via upload

---
+++ Q11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	float a,c;
+	cout<<"Enter the radius of a circle:";
+	cin>>r;
+	
+	a=3.14*r*r;
+	c=2*3.14*r;
+	cout<<"The Area of circle is : "<<a;
+	cout<<"\nThe Circumference of circle is : "<<c<< endl;
+	cout<<"Name: Ansh Sharma"<<endl<<"Enrollment no: 07829802021"<<endl;
+	return 0;
+}
+
