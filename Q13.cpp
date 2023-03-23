79431bf45ada857c1b6bb4a64582c2539f3af5e8
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-03-23 23:34:55 +0530

    Add files via upload

---
+++ Q13.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,fact=1;
+	cout<<"Enter number to calculate for factorial: ";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+		fact*=i;
+	cout<<"The factorial of "<<n<<" is : "<<fact<<endl;
+	cout<<"Name: Ansh Sharma"<<endl<<"Enrollment no: 07829802021"<<endl;
+    return 0;
+}
+
