cbd18b65050772d5e2f8d474eca26bd7c4ee6ae3
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-19 20:50:24 +0530

    Update Q8.cpp

--- Q8.cpp
+++ Q8.cpp
@@ -1,12 +1,17 @@
 #include<iostream> 
 using namespace std;
+
+
 int main()
 {
+
 	int a,c,b,choice,value;
 	c=1;
+
 	while(c){
 		cout<<"Enter the values of two operands: ";
 		cin>>a>>b;
+
 		cout<<endl<<"Enter operant (+,-,*,/,%) you want to perform between operands:"<<endl;
 		cout<<"Press 1 for Addition"<<endl;
 		cout<<"Press 2 for Multiplication"<<endl;
@@ -15,20 +20,26 @@ int main()
 		cout<<"Press 5 for Subtraction"<<endl;
 		cout<<"Press 6 for Exit"<<endl;
 		cin>>choice;
+
 		switch(choice)
 		{
+
 			case 1:
 				cout<<"The addition of "<<a<<" and "<<b<<" is "<<a+b<<endl;
 				break;
+
 			case 2:
 				cout<<"The multiplication of "<<a<<" and "<<b<<" is "<<a*b<<endl;
 				break;
+
 			case 3:
 				cout<<"The division of "<<a<<" and "<<b<<" is "<<a/b<<endl;
 				break;	
+
 			case 4:
 				cout<<"The remainder of "<<a<<" and "<<b<<" is "<<a%b<<endl;
 				break;
+
 			case 5:
 				cout<<"The substraction of "<<a<<" and "<<b<<" is "<<a-b<<endl;
 				break;
