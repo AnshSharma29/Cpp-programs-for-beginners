6f4fd2d6ac6c37eea3e58b077c5828eb84ac9b88
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-21 23:20:45 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,13 +1,19 @@
+
 #include<iostream>
 using namespace std;
+
 int main()
+
 {
 	
 	float s,p,r,t;
+
 	cout<<"Enter the value of  Principal, Rate and Time respectively: ";
+
 	cin>>p>>r>>t;
 	
 	s=0.01*p*r*t;
+
 	cout<<"The Simple Interest is : "<<s<<endl;
     
     return 0;
