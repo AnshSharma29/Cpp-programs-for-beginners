770f4400af321d3a7bb6440b6e45e6553f0a94dc
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-23 01:13:17 +0530

    Update Q13.cpp

--- Q13.cpp
+++ Q13.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
+
 	int n,fact=1;
 	cout<<"Enter number to calculate for factorial: ";
+
 	cin>>n;
+
 	for(int i=1;i<=n;i++)
 		fact*=i;
 	cout<<"The factorial of "<<n<<" is : "<<fact<<endl;
