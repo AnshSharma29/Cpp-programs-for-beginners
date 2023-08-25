554aa8504e60ca5f044c474a6a8139b67657fc4b
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-25 20:01:44 +0530

    Update Q14.cpp

--- Q14.cpp
+++ Q14.cpp
@@ -1,11 +1,17 @@
+
+
 #include<iostream>
 using namespace std;
+
+
 int main()
 {
 	int n,n1=0,n2=1,next;
 	cout<<"Enter the number of terms upto which you want to display the fibonacci series: ";
 	cin>>n;
+
 	cout<<n1<<" "<<n2<<" ";
+
 	for(int i=3;i<=n;i++)
 	{
 		next=n1+n2;
