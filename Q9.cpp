6a566b3120fde2b33fff9d9341024dd662cdceca
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-27 18:31:42 +0530

    Update Q9.cpp

--- Q9.cpp
+++ Q9.cpp
@@ -1,14 +1,20 @@
+
+
 #include<iostream>
 using namespace std;
+
 int main()
 {
 	int a,b;
 	a=8;
 	b=6;
 	b=++a;
+
 	cout<<"For pre-increment : The value of a is "<<a<<" and b is "<< b<<endl;
+
 	a=7;
 	b=a++;
+
 	cout<<"For post-increment : The value of a is "<<a<<" and b is "<< b<<endl;
 	
 	return 0;	
